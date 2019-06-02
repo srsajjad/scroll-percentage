65df6a61cacb371ca3cdf2d045d6d7bc19f9c888
srsajjad <sajjadsr001@gmail.com>
2019-06-02 20:59:27 +0600

    add npm and unpkg setup

---
+++ main.js
@@ -0,0 +1,26 @@
+function calculateScrollPercentage (target, childElemHeight) {
+  try {
+    if (!target) throw 'scrolling element height not found'
+
+    let bool = target === document
+
+    let scrollHeight = (() => {
+      if (bool) return childElemHeight
+      return target.scrollHeight
+    })()
+
+    if (!scrollHeight) throw 'child element height not found'
+
+    let clientHeight = bool ? window.innerHeight : target.clientHeight
+    let scrollTop = bool ? window.scrollY : target.scrollTop
+
+    let gottaScroll = scrollHeight - clientHeight
+    let percentage = Math.ceil((scrollTop / gottaScroll) * 100)
+
+    return percentage
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export { calculateScrollPercentage }
